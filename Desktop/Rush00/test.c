a393d3555bdb6d9a29f500cd08e987fc7c4b0cb0
Leonard Gaillard <lgaillar@c2r9s3.42lausanne.ch>
2021-07-10 18:53:35 +0200

    Negatif

--- Desktop/Rush00/test.c
+++ Desktop/Rush00/test.c
@@ -1,78 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaillar <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/10 17:35:21 by lgaillar          #+#    #+#             */
+/*   Updated: 2021/07/10 18:52:36 by lgaillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaillar <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/10 17:35:13 by lgaillar          #+#    #+#             */
+/*   Updated: 2021/07/10 17:35:13 by lgaillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_HandT(int x, int y);
 void	ft_middle(int x, int y);
+void	ft_putchar(char	c);
 
 void test(int x, int y)
 {
-	char	c;
 	int reset;
 
+	if ( x < 0)
+		x = x * -1;
+	if ( y < 0)
+		 y = y * -1;
 	reset = x;
-	c = 'B';
 	ft_HandT(x, y);
-	ft_middle(x, y);
+	if( y > 1)
+	{
+		ft_middle(x, y);
+		write(1, "\n", 1);
+		ft_HandT(x, y);
+	}
 	write(1, "\n", 1);
-	ft_HandT(x, y);
 }
 
 void ft_HandT(int x, int y)
 {
-		int	reset;
-	char	c;
+	int	reset;
 
-	c = 'B';
 	reset = x;
-	while ( x > 0 )
+	while ( x > 0 || x < 0)
 	{
 			if ( x == reset)
-			{
-				c--;
-				write(1, &c, 1);
-				c++;
-			}
+				ft_putchar('A');
 			else if (x == 1)
-			{
-				c++;
-				write(1, &c, 1);
-				c--;
-			}
+				ft_putchar('C');
 			else
-			{
-				write(1, &c, 1);
-			}
+				ft_putchar('B');
 			x--;
 	}
 }
 void	ft_middle(int x, int y)
 {
 	int	reset;
-	char	c;
+	char c;
 
 	c = 'B';
 	reset = x;
 	while ( y > 2)
 		{
 				write(1, "\nB", 2);
-				x = reset;
 				while ( x > 2)
 				{
 					write (1, " ", 1);
 					x--;
 				}
-			
-			
-				while ( x > 2)
+				x = reset;
+				if ( x > 1)
 				{
-					write(1, &c, 1);
-					x--;
+				write(1, &c, 1);
 				}
-			
-			write(1, "B", 1);
+					;
 			y--;
 		}
 }
 int main()
 {
-	test(4, 5);
+	test(-12, -12);
 }
